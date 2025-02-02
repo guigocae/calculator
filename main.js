18b5aaca9b03bd4195f70da07b3719e26ad61822
guigocae <117747661+guigocae@users.noreply.github.com>
2025-02-02 12:46:25 -0300

    Calculadora funcionando
    próximos passos: permitir contas com mais de dois operadores

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// relógio
 function getTimeNow(){
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
     const customDateTime = new Date().toLocaleString('pt-BR', options);
@@ -6,6 +7,13 @@ function getTimeNow(){
     return(customTimeSlice);
 }
 
+const relogio = document.getElementById("time");
+const intervalTime = setInterval(() => {
+    let time = getTimeNow();
+    relogio.innerHTML = time;
+}, 100)
+
+//lógica da calculadora
 let firstNumber, operator, lastNumber;
 
 function operate(operator, number1, number2){
@@ -29,7 +37,6 @@ function operate(operator, number1, number2){
     return result;
 }
 
-
 function add(number1, number2){
     return number1+number2;
 }
@@ -47,3 +54,42 @@ function divide(number1, number2){
         return "error";
     return number1/number2;
 }
+
+const display = document.querySelector("#display-number");
+const arm = document.querySelector(".arm");
+
+function displayExpression(digit){
+    if(digit === "clear"){
+        display.innerHTML = "0";
+        arm.innerHTML = "";
+        return;
+    }
+    if(display.textContent.length >= 8) return;
+    if(display.textContent === "0"){
+        display.innerHTML = digit;
+        return;
+    }
+    display.innerHTML += digit;
+}
+
+function armFirst(oper) {
+    firstNumber = +display.textContent;
+    
+    operator = oper;
+    
+    arm.innerHTML = `${firstNumber} ${operator} `;
+    display.innerHTML = "0";
+}
+
+function getResult() {
+    if(arm.textContent.includes("=")) return;
+
+    lastNumber = +display.textContent;
+    let result = operate(operator, firstNumber, lastNumber);
+
+    arm.innerHTML += `${lastNumber} =`;
+
+    display.innerHTML = result;
+}
+
+
